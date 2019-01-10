3913c3e62a755f2cb7b0e19cf515fbaf4fe142d3
Sebastian Haas <haa17482@spengergasse.at>
2019-01-10 08:19:36 +0100

    Dao, Persistable and DaoPatient created and minor changes in Patient

--- src/main/java/at/spengergasse/haas/pos/planner/DaoPatient.java
+++ src/main/java/at/spengergasse/haas/pos/planner/DaoPatient.java
@@ -7,19 +7,15 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 import at.spengergasse.haas.pos.planner.Patient;
 
-public class DaoPatient implements Dao<Patient, Long>{
+public class DaoPatient implements Dao<Patient, Long> {
 
     private Connection connection;
 
-    public DaoPatient(Connection connection){
+    public DaoPatient(Connection connection) {
         this.connection = connection;
-    }
-
-    @Override
-    public Patient findById(Long id) {
-        return null;
     }
 
     @Override
@@ -29,22 +25,23 @@ public class DaoPatient implements Dao<Patient, Long>{
         try {
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("SELECT * from Patient");
-            while(rs.next()){
+            while (rs.next()) {
                 Long id = rs.getLong("id");
                 String firstname = rs.getString("firstname");
                 String lastname = rs.getString("lastname");
-                LocalDate birthday = rs.getObject("birthday",LocalDate.class);
+                LocalDate birthday = rs.getObject("birthday", LocalDate.class);
                 int age = rs.getInt("age");
                 float height = rs.getFloat("height");
                 float weight = rs.getFloat("weight");
-                var type= Type.valueOf(rs.getString("type"));
+                var type = Type.valueOf(rs.getString("type"));
 
                 Patient r = new Patient();
+                r.createPatient(firstname,lastname,birthday,age,height,weight,type);
                 r.afterInsert(id);
                 result.add(r);
             }
 
-        } catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
@@ -53,11 +50,69 @@ public class DaoPatient implements Dao<Patient, Long>{
 
     @Override
     public Patient save(Patient persistable) {
-        return null;
+        Patient r = new Patient();
+        if(persistable.getId()==null){
+
+        }
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * from Patient where id=");
+            while (rs.next()) {
+                Long id = rs.getLong("id");
+                String firstname = rs.getString("firstname");
+                String lastname = rs.getString("lastname");
+                LocalDate birthday = rs.getObject("birthday", LocalDate.class);
+                int age = rs.getInt("age");
+                float height = rs.getFloat("height");
+                float weight = rs.getFloat("weight");
+                var type = Type.valueOf(rs.getString("type"));
+
+
+                r.createPatient(firstname,lastname,birthday,age,height,weight,type);
+                r.afterInsert(id);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return r;
     }
 
     @Override
     public Patient delete(Patient persistable) {
         return null;
+    }
+
+    @Override
+    public Patient findById(Long idPersistable) {
+
+
+        Patient r = new Patient();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * from Patient where id=" + idPersistable);
+            while (rs.next()) {
+                Long id = rs.getLong("id");
+                String firstname = rs.getString("firstname");
+                String lastname = rs.getString("lastname");
+                LocalDate birthday = rs.getObject("birthday", LocalDate.class);
+                int age = rs.getInt("age");
+                float height = rs.getFloat("height");
+                float weight = rs.getFloat("weight");
+                var type = Type.valueOf(rs.getString("type"));
+
+
+                r.createPatient(firstname,lastname,birthday,age,height,weight,type);
+                r.afterInsert(id);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return r;
     }
 }
