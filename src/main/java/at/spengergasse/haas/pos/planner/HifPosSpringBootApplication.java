5eed69a8c27935bf9d42c1a9d95a0589b44327b3
sebastian <haa17482@spengergasse.at>
2019-04-12 15:25:41 +0200

    new Dependencies, Main class made

---
+++ src/main/java/at/spengergasse/haas/pos/planner/HifPosSpringBootApplication.java
@@ -0,0 +1,14 @@
+package at.spengergasse.haas.pos.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HifPosSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HifPosSpringBootApplication.class, args);
+    }
+
+}
